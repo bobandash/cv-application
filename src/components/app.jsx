d8b59e1d252202d5f35dbe731c87d39b3bcedd21
Bruce Hsu <brucehsu1126@gmail.com>
2023-08-14 22:56:52 -0700

    added app logic to work experience

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,32 +1,57 @@
 import FormContainer from './container-form'
 import GeneralInfoForm from './general-info-form'
-import ResumeContainer from './container-resume'
+import {ResumeContainer, ResumeContentContainer} from './container-resume'
 import GeneralInfoText from './general-info-text'
-
+import WorkExperienceForm from './work-experience-form'
+import '@fortawesome/fontawesome-free'
+import "@fortawesome/fontawesome-free/css/all.min.css";
 import { useState } from 'react';
+import { v4 as uuid} from 'uuid';
+import { workExperienceData, generalInfoData } from './sample-data'
+import SectionHeader from './section-header'
+import WorkExperienceText from './work-experience-text'
 
 function App(){
-  const [generalInfo, setGeneralInfo] = useState({
-    name: "Bruce Hsu",
-    email: "brucescreation@gmail.com",
-    phoneNumber: "911",
-    cityAndState: "Los Angeles, CA"
-  });
+  const [generalInfo, setGeneralInfo] = useState(generalInfoData);
+  const [currentWorkExperience, setCurrentWorkExperience] = useState({})
+  const [workExperience, setWorkExperience] = useState(workExperienceData)
 
-  function handleInput(e){
+  function handleGeneralInfoInput(e){
     const value = e.target.value;
     const prop = e.target.name;
     setGeneralInfo({...generalInfo, [prop]: value})
   }
-  
 
+  function handleWorkExperienceInput(e) {
+    const value = e.target.value;
+    const prop = e.target.name;
+    setCurrentWorkExperience({...currentWorkExperience, [prop]: value})
+  }
+
+  // when the finish button is clicked on, the work experience is added
+  function handleSetWorkExperience(){
+    const newWorkExperience = {...currentWorkExperience, id: uuid()};
+    setWorkExperience([...workExperience, newWorkExperience]);
+    console.log(workExperience);
+    setCurrentWorkExperience({});
+  }
+
+  const hasWorkExperience = ((Object.keys(currentWorkExperience).length > 0 || workExperience.length > 0) ? true : false)
   return (
     <>
     <FormContainer>
-      <GeneralInfoForm handleInput = {handleInput} props = {generalInfo}/>
+      <GeneralInfoForm handleInput = {handleGeneralInfoInput} props = {generalInfo}/>
+      <WorkExperienceForm workExperience = {workExperience} handleSetWorkExperience = {handleSetWorkExperience} handleInput = {handleWorkExperienceInput} currentWorkExperience = {currentWorkExperience}/>
     </FormContainer>
     <ResumeContainer>
       <GeneralInfoText {...generalInfo} />
+      <ResumeContentContainer>
+          {hasWorkExperience && <SectionHeader text = "Work Experience" />}
+          {workExperience.map(job => (
+            <WorkExperienceText key = {job.id} {...job} />
+          ))}
+          <WorkExperienceText {... currentWorkExperience} />
+      </ResumeContentContainer>
     </ResumeContainer>
     </>
   )
