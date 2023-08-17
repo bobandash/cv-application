d07876aa3cbe60a82490e4ab4d6d2408282db33f
Bruce Hsu <brucehsu1126@gmail.com>
2023-08-17 10:16:27 -0700

    add sample data for clear data

---
+++ src/components/sample-empty-data.jsx
@@ -0,0 +1,7 @@
+const emptyWorkExperienceData = [];
+const emptyGeneralInfoData = {};
+const emptyEducationData = {}
+const emptySkillsInterestsData = {};
+const emptyCurrentWorkExperienceData = {};
+
+export {emptyWorkExperienceData, emptyGeneralInfoData, emptyEducationData, emptySkillsInterestsData, emptyCurrentWorkExperienceData}
