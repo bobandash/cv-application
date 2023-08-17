87d804946b9150d2f4435812a12474f8ed196864
Bruce Hsu <brucehsu1126@gmail.com>
2023-08-17 10:58:59 -0700

    modified sample data for fun

--- src/components/sample-data.jsx
+++ src/components/sample-data.jsx
@@ -1,48 +1,59 @@
 import { v4 as uuid} from 'uuid';
 
+/* made sample data a super stacked resume with a ton of buzz words */
 const workExperienceData = [
   {
-    companyName: 'Amazon',
-    jobTitle: 'Chief Executive Officer',
-    duration: 'August 14, 2023 - August 14, 2023',
+    companyName: 'MeTube',
+    jobTitle: 'CEO',
+    duration: 'Dec. 2022 - Present',
     cityAndState: 'Seattle, Washington',
-    bulletPoints: 'Led 1M people to handle logistics and innovate faster shipping methods to increase customer experience',
+    bulletPoints: 'Founded a YC-backed startup focused on signing Vlog-centric creators. Grew platform 50% month over month with over 80% retention. Raised $5B in VC-funding to date.',
     id: uuid(),
     isEditing: false,
     isHidden: false
   },
   {
-    companyName: 'Apple',
-    jobTitle: 'Chief Executive Officer',
-    duration: 'August 14, 2023 - August 14, 2023',
-    cityAndState: 'Seattle, Washington',
-    bulletPoints: 'Led 1M people to handle logistics and innovate faster shipping methods to increase customer experience',
+    companyName: 'Citadel',
+    jobTitle: 'Quantitative Analyst Intern',
+    duration: 'June 2021 - Aug. 2021',
+    cityAndState: 'New York City, NY',
+    bulletPoints: 'Developed trading algorithims that achieved 700% return in 1 month',
     id: uuid(),
     isEditing: false,
     isHidden: false
+  },
+  {
+    companyName: 'McDonalds',
+    jobTitle: 'Cashier',
+    duration: 'Dec. 2018 - Dec. 2019',
+    cityAndState: 'Seattle, Washington',
+    bulletPoints: 'Processed hundreds of microtransactions every day in a fast-paced environment',
+    id: uuid(),
+    isEditing: false,
+    isHidden: false    
   }
 ]
 
 const generalInfoData = {
-  name: "Bruce Hsu",
-  email: "brucescreation@gmail.com",
+  name: "John Doe",
+  email: "johndoe@professionalemail.com",
   phoneNumber: "111-111-1111",
-  cityAndState: "Los Angeles, CA"
+  cityAndState: "San Francisco, CA"
 }
 
 const educationData = {
-  universityName: 'USC',
-  degree: 'Political Science',
-  graduationDate: "October 2022",
-  honorsAwards: 'Random Award',
-  studentOrganizations: 'Basketball Club',
-  location: 'Los Angeles, CA',
+  universityName: 'Harvard University',
+  degree: 'Bachelors of Science in Political Science',
+  graduationDate: "December 2022",
+  honorsAwards: 'Gates Scholarship, Mock Trial 1st Place National Winner',
+  studentOrganizations: 'Basketball Club Team Captain, Rowing Team Captain',
+  location: 'Boston, MA',
   gpa: '4.0/4.0'
 }
 
 const skillsInterestsData = {
-  skills: 'ReactJS, VanillaJS, HTML/CSS',
-  interests: 'Youtube, Twitch, Drawing'
+  skills: 'Fluent in English, Mandarin, French, Spanish, Japanese, Korean',
+  interests: 'Anime, Drawing, Rap, Content Creation, Valorant'
 }
 
 export {workExperienceData, generalInfoData, educationData, skillsInterestsData}
